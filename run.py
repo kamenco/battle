97f9b821514287ed22c1fcb4473ba6928fe4b94a
kamenco <85615294+kamenco@users.noreply.github.com>
2024-02-19 12:17:15 +0000

    Update run.py

--- run.py
+++ run.py
@@ -2,12 +2,23 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+
+"""
+GAME EXPLANATION
+The computer will generate 5 ships on a board and 
+and then the player'll guess where those 5 ships are located.
+
+The player has one board GUESS_BOARD, where he will make his the guesses. 
+The other board HIDDEN_BOARD is where the computer 
+will generate randomly 5 ships, each in one cell.
+
+This is one player game, and it will have only
+two boards.
+"""
 # Legend
 # 'X' for placing battleship and hit battleship
 # ' ' for available space
-# ' ' for missed shot
-
-
+# '-' for missed shot
 
 from random import randint
 
@@ -20,22 +31,25 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 This board holds enemy ships
 """
 
-letters_to_numbers = {'A': 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7}
+lsns = {'A': 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7}
 """
 Convert letters to numbers
 """
 def print_board(board):
+    #print first column which is the header
     print("  A B C D E F G H")
-    print('  ----------------')
+    print('  ---------------')
     row_number = 1
     # initializer
+    #%d for decimal, %s for string
+    #% means format
     for row in board:
-        print("%d|%s|" % (row_number, "|".join(row)))
-    
+        print("%d|%s|" % (row_number, "|".join(row)))  
     # for each row we are iterating we join the pipe as seperator
-    
-    row_number += 1
-
+        row_number += 1
+"""
+Starting the game by creating ships randomly
+"""
 def create_ships(board):
     
     # Generates shipds randomly
@@ -50,19 +64,17 @@ def create_ships(board):
     # set it to 'X'
 
 def get_ship_location():
-    
-    
     # Asks the user what row and what column the ship is
-     
     row = input('Please enter a ship row 1-8: ')
-    while row not in '12345678':
+   
+    while row not in '12345678' or row in " " :
         print("Please enter a valid row")
         row = input('Please enter a ship row 1-8: ')
-    column = input("Please ente a ship column A-H: ").upper()
-    while column not in 'ABCDEFGH':
+    column = input("Please enter a ship column A-H: ").upper() 
+    while column not in 'ABCDEFGH' or column in " " :
         print("Please enter a valid column")
-        column = input("Please ente a ship column A-H: ").upper()
-    return int(row) - 1, letters_to_numbers[column]
+        column = input("Please enter a ship column A-H: ").upper()
+    return int(row) - 1, lsns[column]
     # Wrap this in try and except because if user doesnt put anything
     # the program will crash 
 
@@ -70,6 +82,7 @@ def get_ship_location():
 def count_hit_ships(board):
     # Checking if the ship exists
     # Goes through each row and each column
+    # If we hit all five the game is over
     count = 0
     for row in board:
         for column in row:
