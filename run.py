638fa82dfcf729ee94ae3a7ac69402a43ae4dd1d
kamenco <85615294+kamenco@users.noreply.github.com>
2024-04-06 09:26:43 +0300

    Update run.py
    Identation and errore fixed with CI Python Linter Code Institute.

--- run.py
+++ run.py
@@ -33,60 +33,66 @@ This board holds enemy ships
 """
 
 lettersnumbers = {
-    'A': 0, 'B': 1, 'C': 2, 'D': 3, 
+    'A': 0, 'B': 1, 'C': 2, 'D': 3,
     'E': 4, 'F': 5, 'G': 6, 'H': 7
     }
 """
 Convert letters to numbers
 """
+
+
 def print_board(board):
-    #print first column which is the header
+
     print("  A B C D E F G H")
     print('  ---------------')
     row_number = 1
     # initializer
-    #%d for decimal, %s for string
-    #% means format
+    # %d for decimal, %s for string
+    # % means format
     for row in board:
-        print("%d|%s|" % (row_number, "|".join(row)))  
+        print("%d|%s|" % (row_number, "|".join(row)))
     # for each row we are iterating we join the pipe as seperator
         row_number += 1
+
+
 """
 Starting the game by creating ships randomly
 """
+
+
 def create_ships(board):
-    
+
     # Generates shipds randomly
-    
+
     for ship in range(5):
         ship_row, ship_column = randint(0, 7), randint(0, 7)
-        while board[ship_row] [ship_column] == 'X':
-    # check if 'X' exists
+        while board[ship_row][ship_column] == 'X':
+
             ship_row, ship_column = randint(0, 7), randint(0, 7)
-        board [ship_row][ship_column] = 'X'
+        board[ship_row][ship_column] = 'X'
 
     # set it to 'X'
 
-def get_ship_location(): 
-     
-        try:
-            # Asks the user what row and what column the ship is 
-            row = input('Please enter a ship row 1-8: ') 
-            while row not in '12345678': 
-                print("Please enter a valid row") 
-                row = input('Please enter a ship row 1-8: ') 
-            column = input("Please enter a ship column A-H: ").upper() 
-            
-            while column not in 'ABCDEFGH': 
-                print("Please enter a valid column") 
-                column = input("Please enter a ship column A-H: ").upper() 
-            # Use a dictionary to map columns to indexes 
-        
-            return int(row) - 1, lettersnumbers[column] 
-        except KeyError as ex: 
+
+def get_ship_location():
+
+    try:
+        row = input('Please enter a ship row 1-8: ')
+        while row not in '12345678':
+            print("Please enter a valid row")
+            row = input('Please enter a ship row 1-8: ')
+            column = input("Please enter a ship column A-H: ").upper()
+
+        while column not in 'ABCDEFGH':
+            print("Please enter a valid column")
+            column = input("Please enter a ship column A-H: ").upper()
+            # Use a dictionary to map columns to indexes
+
+            return int(row) - 1, lettersnumbers[column]
+        except KeyError as ex:
             print(f"An unexpected error occurred: {ex}. Please try again.")
     # Wrap this in try and except because if user doesnt put anything
-    # the program will crash 
+    # the program will crash
 
 
 def count_hit_ships(board):
@@ -99,6 +105,7 @@ def count_hit_ships(board):
             if column == 'X':
                 count += 1
     return count
+
 
 create_ships(HIDDEN_BOARD)
 print_board(HIDDEN_BOARD)
@@ -123,4 +130,5 @@ while turns > 0:
     print('You have ' + str(turns) + ' turns remaining')
     if turns == 0:
         print('Sorry, you ran out of terms, the game is over')
+
         break
